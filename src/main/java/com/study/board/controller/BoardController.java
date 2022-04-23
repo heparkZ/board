10bfd68676016a09039c9a1209fe308a5243c326
hepark <hellop731@gmail.com>
2022-04-23 22:14:24 +0900

    전송된 데이터 DB에 저장

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,5 +1,8 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping("/board/write") // localhost:8080/board/write 이 주소로 접속하면 아래 boardwrite 폼을 보여준다.
     public String boardWriteForm() {
 
@@ -15,7 +21,9 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(String title, String content) {
+    public String boardWritePro(Board board) {
+
+        boardService.write(board);
 
         return "";
     }
